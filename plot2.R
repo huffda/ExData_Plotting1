6ea6d07591c6309a23f98c9e8ecc9bb10f704963
huffda <davidhuff4@gmail.com>
2015-06-08 11:48:55 -0400

    update to plot2

--- plot2.R
+++ plot2.R
@@ -7,12 +7,13 @@ plot2 <- function() {
   dt <- mutate(dt, Date2 = as.Date(Date, "%d/%m/%Y"))
   wdt <- dt[Date2 %between% c(as.Date("2007-02-01"), as.Date("2007-02-02"))] %>%
     mutate(Global_active_power = as.numeric(Global_active_power)) %>%
-    mutate(fulltime = strptime(paste(Date, Time, sep = " "), "%d/%m/%Y% %T"))
+    mutate(fulltime = strptime(paste(Date, Time, sep = " "), "%d/%m/%Y% %T")))
   
-  png("plot1.png", width = 480, height = 480)
+  png("plot2.png", width = 480, height = 480)
   hist(wdt$Global_active_power, col = 'red', xlab = "Global Active Power (kilowatts)", main="Global Active Power")
   dev.off()
 }
+dateTime   <- as.POSIXlt(paste(as.Date(data$Date, format="%d/%m/%Y"), data$Time, sep=" "))
 
 wdt3 <- mutate(wdt2, fulltime = strptime(datetime, "%d/%m/%Y %H:%M:%S"))
 wdt3 <- mutate(wdt2, fulltime = strptime(datetime, "%d/%m/%Y, %H:%M:%S"))
